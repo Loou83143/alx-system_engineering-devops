1c77bb535512885452db39f2aae1cb7923d6d5d1
Loou83143 <tendohchivas@gmail.com>
2024-02-17 02:42:39 -0800

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and returns the number
-of subscribers (not active users, total subscribers)
-for a given subreddit. If an invalid subreddit is given,
-the function should return 0.
-"""
+"""0-subs module"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns number of total subscribers"""
-    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    queries the Reddit API
 
-    if response.status_code != 200:
-        return (0)
-    response = response.json()
-    if 'data' in response:
-        return (response.get('data').get('subscribers'))
-
-    else:
-        return (0)
+    Returns:
+        the number of subscribers for a given subreddit,
+        0 if an invalid subreddit is given
+    """
+    URL = 'https://www.reddit.com/r/'
+    res = requests.get('{}{}/about.json'.
+                       format(URL, subreddit),
+                       headers={'User-Agent': 'ALX-User-Agent'},
+                       allow_redirects=False)
+    if res.status_code != 200:
+        return 0
+    return res.json().get('data').get('subscribers')
--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,55 +1,32 @@
 #!/usr/bin/python3
-"""function to count words in all hot posts of a given Reddit subreddit."""
+"""100-count module"""
 import requests
 
 
-def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
-
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
+def count_words(subreddit, word_list=[], word_count={}, after=None):
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    try:
-        results = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
-        return
+    queries the Reddit API recursively
 
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
-        for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
-                else:
-                    instances[word] += times
+    Returns:
+        Prints a sorted count of given keywords
+        None if an invalid subreddit is given
+    """
+    URL = 'https://www.reddit.com/r/'
+    res = requests.get('{}{}/hot.json'.
+                       format(URL, subreddit),
+                       params={'after': after},
+                       headers={'User-Agent': 'ALX-User-Agent'},
+                       allow_redirects=False)
 
-    if after is None:
-        if len(instances) == 0:
-            print("")
-            return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
-    else:
-        count_words(subreddit, word_list, instances, after, count)
+    if res.status_code >= 300:
+        return None
+
+    list = hot_list + [child.get('data').get('title')
+                        for child in res.json().get('data').get('children')]
+
+    data = res.json()
+    if not data.get('data').get('after'):
+        return list
+
+    return recurse(subreddit, list, data.get('data').get('count'),
+                   data.get('data').get('after'))
